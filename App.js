f125a46be5d48069bd185bea40baf0b41b85923c
Majomon <mauri.monzon91@gmail.com>
2023-07-29 19:14:27 -0300

    Terminado app

--- App.js
+++ App.js
@@ -1,19 +1,64 @@
 import * as React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import Card from "./Card";
 
 const cards = ["😃", "🚀", "🌈", "🐶", "🍕", "🐬"];
 
 export default function App() {
   const [board, setBoard] = React.useState(() => shuffle([...cards, ...cards]));
+  const [selectedCards, setSelectedCards] = React.useState([]);
+  const [matchedCards, setMatchedCards] = React.useState([]);
+  const [score, setScore] = React.useState(0);
 
+  React.useEffect(() => {
+    if (selectedCards.length < 2) return;
+    if (board[selectedCards[0]] === board[selectedCards[1]]) {
+      setMatchedCards([...matchedCards, ...selectedCards]);
+      setSelectedCards([]);
+    } else {
+      const timeOutId = setTimeout(() => {
+        setSelectedCards([]);
+      }, 1000);
+      return () => clearTimeout(timeOutId);
+    }
+  }, [selectedCards]);
+
+  const handleTapCard = (index) => {
+    if (selectedCards.length >= 2 || selectedCards.includes(index)) return;
+    setSelectedCards([...selectedCards, index]);
+    setScore(score + 1);
+  };
+
+  const didPlayerWin = () => matchedCards.length === board.length;
+  const resetGame = () => {
+    setMatchedCards([]);
+    setScore(0);
+    setSelectedCards([]);
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Memo</Text>
-      {board.map((card, index) => {
-        return <Card key={index}>{card}</Card>;
-      })}
+      <Text style={styles.text}>
+        {didPlayerWin() ? "Bien hecho Janna 😃" : "Memory"}
+      </Text>
+      <Text style={styles.text}>Score: {score}</Text>
+
+      <View style={styles.board}>
+        {board.map((card, index) => {
+          const isTurnedOver =
+            selectedCards.includes(index) || matchedCards.includes(index);
+          return (
+            <Card
+              key={index}
+              isTurnedOver={isTurnedOver}
+              onPress={() => handleTapCard(index)}
+            >
+              {card}
+            </Card>
+          );
+        })}
+      </View>
+      {didPlayerWin() && <Button onPress={resetGame} title="reset" />}
       <StatusBar style="light" />
     </View>
   );
@@ -31,6 +76,11 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 32,
     fontWeight: "900",
+  },
+  board: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
   },
 });
 
